801d54b955e1ad40864031ca009c8c598451cee2
KRSBX <firdaus.sati51@gmail.com>
2022-08-13 21:15:18 +0700

    feat: editable post/comment

--- components/general/posts/PostRead.tsx
+++ components/general/posts/PostRead.tsx
@@ -1,23 +1,59 @@
-import { Box, Flex, Link as ChakraLink, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Link as ChakraLink, Text } from '@chakra-ui/react';
 import moment from 'moment';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { createRef, useEffect, useMemo, useState } from 'react';
+import { MdEdit } from 'react-icons/md';
+import { connect, ConnectedProps } from 'react-redux';
 import { Markdown } from '..';
 import useCardColorMode from '../../../hooks/useCardColorMode';
-import { RESOURCE_NAME } from '../../../utils/constant';
-import { ResourceMap } from '../../../utils/interfaces';
+import useCurrentUserId from '../../../hooks/useCurrentUserId';
+import useIsHoveringElement from '../../../hooks/useIsHoveringElement';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
+import { updateData } from '../../../store/actions/resources';
+import { PLACEHOLDER, RESOURCE_NAME } from '../../../utils/constant';
+import { Post, ReactSetter } from '../../../utils/interfaces';
+import { postSchema } from '../../../utils/schema';
 import { postTheme } from '../../../utils/theme';
 
-const PostRead = ({ post }: Props) => {
+const PostRead = ({ post, setPost, updatePost }: Props) => {
   const createdAt = moment(post?.createdAt).format('DD MMMM YYYY');
   const filter = moment(post?.createdAt);
   const parseDate = (date: moment.Moment) => date.format('YYYY-MM-DD');
 
+  const cardRef = createRef<HTMLDivElement>();
+  const [value, setValue] = useState('');
+  const [isEdit, setIsEdit] = useState(false);
+
+  const currUserId = useCurrentUserId();
+  const isHoveringCard = useIsHoveringElement(cardRef);
   const { cardBgColor, cardTextColor } = useCardColorMode();
 
+  const isEditable = useMemo(() => {
+    if (!post || !currUserId) return false;
+
+    const isAuthor = post.userId === currUserId;
+
+    return isAuthor && isHoveringCard && !isEdit;
+  }, [post?.userId, currUserId, isHoveringCard, isEdit]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useOnClickOutside(cardRef, () => setIsEdit(false));
+
+  useEffect(() => {
+    if (!post) return;
+
+    setValue(post.content);
+  }, [post]);
+
+  const onUpdate = async (values: { content: string }) => {
+    if (!post) return;
+
+    setPost(await updatePost(post?.id, values));
+    setIsEdit(false);
+  };
+
   return (
-    <Box {...postTheme} backgroundColor={cardBgColor} color={cardTextColor}>
-      <Flex gap={3} alignItems="center">
+    <Box {...postTheme} backgroundColor={cardBgColor} color={cardTextColor} ref={cardRef}>
+      <Flex gap={3} alignItems="center" position={'relative'} mb={2}>
         <NextLink href={`/posts?filters=user.username = "${post?.user?.username}"`} passHref>
           <ChakraLink
             _hover={{
@@ -62,14 +98,40 @@ const PostRead = ({ post }: Props) => {
             </Text>
           </ChakraLink>
         </NextLink>
+        {isEditable && (
+          <Button
+            position={'absolute'}
+            right={0}
+            top={0}
+            variant={'ghost'}
+            onClick={() => setIsEdit(true)}
+          >
+            <MdEdit />
+          </Button>
+        )}
       </Flex>
-      <Markdown.Preview value={post?.content ?? ''} />
+      {isEdit ? (
+        <Markdown.PostCommentField
+          value={value}
+          setValue={setValue}
+          schema={postSchema}
+          placeholder={PLACEHOLDER.POST}
+          onSubmit={onUpdate}
+        />
+      ) : (
+        <Markdown.Preview value={post?.content ?? ''} />
+      )}
     </Box>
   );
 };
 
-type Props = {
-  post: ResourceMap[typeof RESOURCE_NAME.POST] | null | undefined;
+const connector = connect(null, {
+  updatePost: updateData(RESOURCE_NAME.POST),
+});
+
+type Props = ConnectedProps<typeof connector> & {
+  post: Post | null;
+  setPost: ReactSetter<Post | null>;
 };
 
-export default PostRead;
+export default connector(PostRead);
